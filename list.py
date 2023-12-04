65596c76a58c96960149cac85468c26cbf481864
20194507 <100979132+20194507@users.noreply.github.com>
2023-12-04 20:45:16 +0900

    Update list.py

--- list.py
+++ list.py
@@ -1,3 +1,5 @@
+# https://www.youtube.com/watch?v=Lqd8o7vL2Z8&list=PLsMufJgu5933ZkBCHS7bQTx0bncjwi4PK&index=6&ab_channel=Chan-SuShin
+
 import sys
 
 A = []
@@ -5,5 +7,3 @@ print(sys.getsizeof(A))
 
 A.append(10)
 print(sys.getsizeof(A))
-
-# https://www.youtube.com/watch?v=Lqd8o7vL2Z8&list=PLsMufJgu5933ZkBCHS7bQTx0bncjwi4PK&index=6&ab_channel=Chan-SuShin
